cb4a9d67e669116e8f794456d534c997c73b672b
Francesco <francesco.ricciardi.24@stud.itsaltoadriatico.it>
2025-01-14 09:46:13 +0100

    magia ricciardi

---
+++ sortation.py
@@ -0,0 +1,14 @@
+# sortation.py
+
+def main():
+    # Creazione di una lista di numeri
+    numeri = [5, 2, 9, 1, 5, 6]
+
+    # Ordinamento della lista in modo crescente
+    numeri_ordinati = sorted(numeri)
+
+    # Stampa della lista ordinata
+    print("Lista ordinata:", numeri_ordinati)
+
+if __name__ == "__main__":
+    main()
